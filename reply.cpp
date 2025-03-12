032f086a07679b68b37b7d1aedc2480fdd9053aa
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-03-12 17:25:17 +0100

    messo namespace, tolti std:: e aggiunto  text file demo

--- reply.cpp
+++ reply.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 struct Risorsa {
     int RI, RA, RP, RW, RM, RL, RU;
     char RT;
@@ -14,16 +16,16 @@ struct Turno {
 };
 
 int main() {
-    std::ifstream inputFile("input.txt"); // Nome del file di input
+    ifstream inputFile("0-demo.txt"); // Nome del file di input
     if (!inputFile) {
-        std::cerr << "Errore nell'apertura del file!" << std::endl;
+        cerr << "Errore nell'apertura del file!" << endl;
         return 1;
     }
 
     int D, R, T;
     inputFile >> D >> R >> T; // Legge i valori iniziali
     
-    std::vector<Risorsa> risorse(R);
+    vector<Risorsa> risorse(R);
     for (int i = 0; i < R; ++i) {
         inputFile >> risorse[i].RI >> risorse[i].RA >> risorse[i].RP >> risorse[i].RW
                   >> risorse[i].RM >> risorse[i].RL >> risorse[i].RU >> risorse[i].RT;
@@ -34,7 +36,7 @@ int main() {
         }
     }
     
-    std::vector<Turno> turni(T);
+    vector<Turno> turni(T);
     for (int i = 0; i < T; ++i) {
         inputFile >> turni[i].TM >> turni[i].TX >> turni[i].TR;
     }
@@ -42,19 +44,19 @@ int main() {
     inputFile.close();
     
     // Output per verificare la corretta lettura
-    std::cout << "Budget iniziale: " << D << "\nNumero risorse: " << R << "\nNumero turni: " << T << "\n";
-    std::cout << "\nElenco risorse:\n";
+    cout << "Budget iniziale: " << D << "\nNumero risorse: " << R << "\nNumero turni: " << T << "\n";
+    cout << "\nElenco risorse:\n";
     for (const auto& r : risorse) {
-        std::cout << "ID: " << r.RI << ", Costo attivazione: " << r.RA
+        cout << "ID: " << r.RI << ", Costo attivazione: " << r.RA
                   << ", Costo manutenzione: " << r.RP << ", Turni attivi: " << r.RW
                   << ", Turni inattivi: " << r.RM << ", Ciclo di vita: " << r.RL
                   << ", Edifici supportati: " << r.RU << ", Tipo effetto: " << r.RT
                   << ", Effetto RE: " << r.RE << "\n";
     }
     
-    std::cout << "\nElenco turni:\n";
+    cout << "\nElenco turni:\n";
     for (const auto& t : turni) {
-        std::cout << "Min edifici: " << t.TM << ", Max edifici: " << t.TX
+        cout << "Min edifici: " << t.TM << ", Max edifici: " << t.TX
                   << ", Profitto per edificio: " << t.TR << "\n";
     }
     
